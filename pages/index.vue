b17fb2d8184eca57339c5a96eef61521169c330f
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2018-11-30 17:29:03 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  section
+    h1.header Nuxt TypeScript Starter
+    .cards
+      Card(
+        v-for="person in people"
+        :key="person.id"
+        :person="person")
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { State } from 'vuex-class'
+import Card from '~/components/Card.vue'
+
+@Component({
+  components: {
+    Card,
+  },
+})
+export default class extends Vue {
+  @State people
+}
+</script>
+
+<style scoped lang="sass">
+.header
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+
+.cards
+  display: flex
+  flex-wrap: wrap
+</style>
